bba0a9a5ab62ed6c2fa916d2b2c4d7a14189c1f5
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-07-28 21:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const changeButton = document.getElementById("change_button")
+const resetButton = document.getElementById("Reset")
+
+// Add evenListener to buttons
+changeButton.addEventListener("click", () => changeColor())
+resetButton.addEventListener("click", () => resetColor())
+
+// Change the color for selected block
+function changeColor(){
+	// alert("Event working fine")
+	
+	resetColor()
+
+	const selectedBlock = document.getElementById("block_id").value
+	const assignedColor = document.getElementById("colour_id").value
+
+	if(selectedBlock < 1 || selectedBlock > 9){
+		alert("Please enter a valid block 1-9")
+		return
+	}
+
+	document.getElementById(selectedBlock).style.backgroundColor = assignedColor
+	// console.log(selectedBlock, assignedColor)
+}
+
+// Reset the color
+function resetColor(){
+	// alert("reset working fine")
+
+	for(let i=1; i<=9; i++){
+		document.getElementById(i.toString()).style.backgroundColor = "transparent"
+	}
+}
